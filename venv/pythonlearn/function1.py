4c5e5126d8e0be454e1ba15813ae5f239fbeaa63
hermes <hermesdifa@hotmail.com>
2019-03-06 22:01:48 +0800

    update function1

--- venv/pythonlearn/function1.py
+++ venv/pythonlearn/function1.py
@@ -42,8 +42,13 @@ print(a.b1())
 print(a.b2())
 
 
-# 类先调用__new__方法，返回该类的实例对象，这个实例对象就是__init__方法的第一个参数self，即self是__new__的返回值
+'''
 # __init__与__new__比较
+# __new__是一个对象实例化的时候所调用的第一个方法
+# 类先调用__new__方法，返回该类的实例对象，这个实例对象就是__init__方法的第一个参数self，即self是__new__的返回值
+'''
+
+
 class B:
     def __init__(self):
         print("this is __init__")
@@ -65,4 +70,37 @@ print(b.__init__())
 print("3--")
 # print(b.__new__())
 print("4--")
-print(b.printS())
+# print(b.printS())
+
+
+class C:
+    def __init__(self, *args, **kwargs):
+        print("init")
+        print("print init function")
+        return print("print return init %s args: %s kwargs: %s",  self.__class__, *args, **kwargs)
+
+    def __new__(cls, *args, **kwargs):
+        print("new")
+        print("print new function ", *args, **kwargs)
+        return print("print new function %s, args: %s, kwargs: %s", cls.__class__, *args, **kwargs)
+
+
+c = C('12', '23')
+print(c.__init__())
+c1 = C('34', '25', "fs")
+# print(c.__new__())
+
+
+class D(object):
+    def __init__(self, *args, **kwargs):
+        print("init %s" % self.__class__)
+
+    def __new__(cls, *args, **kwargs):
+        print("new %s" % cls)
+        return object.__new__(cls, *args, **kwargs)
+
+
+print("object d")
+d = D()
+# print(type(d))
+# print(d.__new__())
