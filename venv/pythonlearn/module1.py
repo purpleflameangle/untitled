13c4eab911250ca6cb0940b9fa29b0352cdb0231
hermes <hermesdifa@hotmail.com>
2019-03-23 21:05:50 +0800

    update module:add url

--- venv/pythonlearn/module1.py
+++ venv/pythonlearn/module1.py
@@ -1,7 +1,12 @@
 # coding = utf-8
 import sys as s
 import timeit as ti
+import urllib.request
 
 print(s.path)
 print(ti.__doc__)
 print(dir(ti))
+
+response = urllib.request.urlopen("http://www.baidu.com")
+html1 = response.read()
+print(html1)
