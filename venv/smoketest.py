0392cc3bcc97fbb2a510708f81f15d8d108ed771
hermes <hermesdifa@hotmail.com>
2019-04-13 21:34:29 +0800

    update

---
+++ venv/smoketest.py
@@ -0,0 +1,10 @@
+import unittest
+from xmlrunner import xmlrunner
+# from pythonlearn import demotest
+
+
+#demo_tests=unittest.TestLoader().loadTestsFromTestCase(demotest)
+
+#smoke_tests = unittest.TestSuite([demo_tests])
+
+#xmlrunner.XMLTestRunner(verbosity=2,output='test-reports').run(smoke_tests)
