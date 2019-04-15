8787d6dd23660e97e8dffaa2181099cb547e16f9
hermes <hermesdifa@hotmail.com>
2019-04-15 19:55:28 +0800

    update urlrequest success code

--- venv/pythonlearn/urloption.py
+++ venv/pythonlearn/urloption.py
@@ -4,20 +4,14 @@ import urllib.request
 import urllib.parse
 import re
 import ssl
+import json
+import sys
 from bs4 import BeautifulSoup
 
 
+print(sys.stdout.encoding)
 '''
-keyword = input("请输入关键字：")
-keyword1 = urllib.parse.urlencode({"word": keyword})
-headers1 = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15'}
-request1 = urllib.request.Request("http://baike.baidu.com/search/word?%s" % keyword1, headers1)
-response2 = urllib.request.urlopen(request1)
-html = response2.read().decode('utf-8')
-'''
-
-# run true
+#  run success one
 key = input("请输入关键字：")
 key = key.encode('utf-8')
 url2 = "http://suggestion.baidu.com/su?wd=%s" % key
@@ -28,32 +22,52 @@ with open('proxy1.html', 'w+') as f:
     f.write(html)
 
 
-'''
+# request run success two
 keyword = input("please input:")
-keyworden = keyword.encode('utf-8')
-keyword1 = urllib.parse.urlencode(keyworden)
-request = urllib.request.Request()
-# url = "http://www.baidu.com/search/word/df"
-response = urllib.request.urlopen("http://www.baidu.com/search/word?%s" % keyword1)
-html = response.read()
-print(html)
+#keyworden = keyword.encode('utf-8')
+# 服务器地址
+url = "http://suggestion.baidu.com/su?wd=%s" % keyword
+request = urllib.request.Request(url)
+response = urllib.request.urlopen(request)
+html1 = response.read().decode('gb2312')
+response.close()
+print(html1)
 
 
 
+#http post header向服务器提交头信息 run success thrid
+keywordheader = input("请输入关键字：")
+urlheader = 'http://www.baidu.com/s?wd=%s' %keywordheader
+context = ssl._create_unverified_context()
+head = {}
+head['Content-Type'] = 'application/json'
+head['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15'
+request1 = urllib.request.Request(url=urlheader, headers=head)
+response2 = urllib.request.urlopen(request1, context=context)
+html = response2.read().decode('utf-8')
+with open('proxy2.html', 'w+') as f:
+    f.write(html)
 
+'''
+
+
+#soup run success four
 def main():
     # url = "http://www.baidu.com"
+    context = ssl._create_unverified_context()
     keyword = input("请输入关键字：")
     keyword1 = urllib.parse.urlencode({"word": keyword})
-    response1 = urllib.request.urlopen("http://baike.baidu.com/search/word?%s" % keyword1)
+    response1 = urllib.request.urlopen("http://baike.baidu.com/search/word?%s" % keyword1, context=context)
     html = response1.read()
-    soup = BeautifulSoup(html, 'html.parser')
+    htmlr = urllib.parse.quote(html)
+    soup = BeautifulSoup(htmlr, 'html.parser')
     for each in soup.find_all(href=re.compile('view')):
         content = ''.join([each.text])
         url2 = ''.join(["http://baike.baidu.com", each['href']])
-        response2 = urllib.request.urlopen(url2)
-        html2 = response2.load()
-        soup2 = BeautifulSoup(html2, "html.parser")
+        response2 = urllib.request.urlopen(url2, context=context)
+        html2 = response2.read()
+        htmlr2 = urllib.parse.quote(html2)
+        soup2 = BeautifulSoup(htmlr2, "html.parser")
         if soup2.h2:
             content = ''.join([content, soup2.h2.text])
         content = ''.join([content, "->", url2])
@@ -62,4 +76,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-'''
+
