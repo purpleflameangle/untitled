4c055b2da8140cfb77c66099354a5f4c3a2ccd54
hermes <hermesdifa@hotmail.com>
2019-04-16 21:10:01 +0800

    add get post annotation

--- venv/pythonlearn/urloption.py
+++ venv/pythonlearn/urloption.py
@@ -10,7 +10,17 @@ from bs4 import BeautifulSoup
 
 
 print(sys.stdout.encoding)
+context = ssl._create_unverified_context()
 '''
+#get 请求： 一般从指定的服务器请求数据，也有的提交数据给服务器
+params = urllib.parse.urlencode({'wd': 'Python get请求'})
+print(params)
+response = urllib.request.urlopen('http://www.baidu.com/s?%s' % params)
+html = response.read().decode('utf-8')
+with open('gethtml.html', 'w+') as f:
+    f.write(html)
+
+
 #  run success one
 key = input("请输入关键字：")
 key = key.encode('utf-8')
@@ -28,13 +38,49 @@ keyword = input("please input:")
 # 服务器地址
 url = "http://suggestion.baidu.com/su?wd=%s" % keyword
 request = urllib.request.Request(url)
+#urlopen的Url参数既可以是字符串也可以是Request对象，如果你传入一个字符串，
+# 那么Python是会默认先帮你把目标字符串转换成Request对象，然后再传给urlopen函数
 response = urllib.request.urlopen(request)
+# urlopen实际上返回的是一个类文件对象，因此你可以用read（）方法来读取内容
+# geturl()  返回请求的url
+# info（） 返回一个httplib.HTTPMessage对象，包含远程服务器返回的头信息
+# getcode() 返回HTTP状态码
 html1 = response.read().decode('gb2312')
 response.close()
 print(html1)
+'''
 
+'''
+get 与post的区别
+http://www.w3school.com.cn/tags/html_ref_httpmethods.asp
+GET 请求可被缓存
+GET 请求保留在浏览器历史记录中
+GET 请求可被收藏为书签
+GET 请求不应在处理敏感数据时使用
+GET 请求有长度限制
+GET 请求只应当用于取回数据
 
+POST 请求不会被缓存
+POST 请求不会保留在浏览器历史记录中
+POST 不能被收藏为书签
+POST 请求对数据长度没有要求
 
+urlopen函数有一个data参数，如果给这个参数赋值，那么HTTP请求就是使用POST方法
+如果data的值是NULL,也就是默认值，那么HTTP的请求就是get方式
+data参数的值必须符合这个application/x-wwww-form-urlencodec的格式。使用urllib.parse.urlencode()将字符串转换为这个格式
+'''
+
+# POST请求：向指定的服务器提交要被处理的数据。
+# 请求头添加字符参数内容
+data1 = urllib.parse.urlencode({'wd': "Python post request "}).encode('utf-8')
+request = urllib.request.Request("http://www.baidu.com/s?")
+request.add_header("Content-Type", "application/json")
+request.add_header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15")
+response = urllib.request.urlopen(url=request, data=data1, context=context)
+# 确定返回的HTTP状态码
+print(response.getcode())
+
+'''
 #http post header向服务器提交头信息 run success thrid
 keywordheader = input("请输入关键字：")
 urlheader = 'http://www.baidu.com/s?wd=%s' %keywordheader
@@ -47,11 +93,12 @@ response2 = urllib.request.urlopen(request1, context=context)
 html = response2.read().decode('utf-8')
 with open('proxy2.html', 'w+') as f:
     f.write(html)
-
 '''
 
 
-#soup run success four
+
+'''
+# soup run success four
 def main():
     # url = "http://www.baidu.com"
     context = ssl._create_unverified_context()
@@ -76,4 +123,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+'''
 
