efab29ad0e24abe407429d7627e8d275a39b74d7
hermes <hermesdifa@hotmail.com>
2019-05-22 21:43:39 +0800

    add quotes_spider.py

---
+++ venv/pythonlearn/scrapy/tutoria/tutoria/spiders/scrapy1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+import os
+import sys
+import urllib
+import urllib.request
+import scrapy
+from tutoria.items import TutoriaItem
+
+
+# 创建一个类，并继承scrapy的一个子类：scrapy.spider或者是其他蜘蛛类型，
+# 除了spider或者其他蜘蛛类型，后面会说到，除了spider还有其他类型
+#
+
+
+
+
+
